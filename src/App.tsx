54eb53cf5fe2d674014ce9dcd6712d5403ab25e0
ahmadibni <ahmadibni007@gmail.com>
2025-06-08 22:39:52 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <main className="bg-gray-100">
+        <header className="">
+          <h1>MakanKi'</h1>
+        </header>
+      </main>
+    </>
+  );
+}
+
+export default App;
