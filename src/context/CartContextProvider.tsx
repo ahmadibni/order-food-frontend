7a94a05e126ffba56aefab735d0bcdc8f93e1946
ahmadibni <ahmadibni007@gmail.com>
2025-06-19 10:38:09 +0700

    modify cart context and add hooks useCart

--- src/context/CartContextProvider.tsx
+++ src/context/CartContextProvider.tsx
@@ -1,10 +1,30 @@
 import { createContext, useState, type ReactNode } from "react";
 import type { CartContextType, CartItem } from "../types/Cart";
 
-export const cartContext = createContext<CartContextType | null>(null);
+export const CartContext = createContext<CartContextType | null>(null);
 
 const CartContextProvider = ({ children }: { children: ReactNode }) => {
-  const [cartItems, setCartItems] = useState<CartItem>();
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (cartItem: CartItem) => {
+    setCartItems((prev) => [...prev, cartItem]);
+  };
+
+  const removeFromCart = (foodId: string) => {
+    setCartItems((prev) => prev.filter((item) => item.foodId !== foodId));
+  };
+
+  const contextValue = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+    totalAmount: cartItems.reduce((acc, item) => acc + item.subtotal, 0),
+    totalItems: cartItems.length,
+  };
+
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
 };
 
 export default CartContextProvider;
