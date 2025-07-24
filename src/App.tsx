4a68dbde999125f103b2450514f34c3a50f315a4
ahmadibni <ahmadibni007@gmail.com>
2025-07-24 14:16:25 +0700

    refactor route using data mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 import RootLayout from "./layouts/RootLayout";
 import FoodsPage from "./pages/foods/FoodsPage";
 import FoodLayout from "./layouts/FoodLayout";
@@ -6,22 +9,46 @@ import FoodDetailPage from "./pages/foods/FoodDetailPage";
 import CreateOrderPage from "./pages/orders/CreateOrderPage";
 import MyOrderPage from "./pages/orders/MyOrderPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        element: <FoodLayout />,
+        children: [
+          { index: true, element: <FoodsPage /> },
+          { path: "foods/:foodId", element: <FoodDetailPage /> },
+        ],
+      },
+      {
+        path: "orders",
+        children: [
+          { index: true, element: <MyOrderPage /> },
+          { path: "add", element: <CreateOrderPage /> },
+        ],
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route element={<FoodLayout />}>
-            <Route index element={<FoodsPage />} />
-            <Route path="/foods/:foodId" element={<FoodDetailPage />} />
-          </Route>
-          <Route path="orders">
-            <Route index element={<MyOrderPage />} />
-            <Route path="add" element={<CreateOrderPage />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<RootLayout />}>
+    //       <Route element={<FoodLayout />}>
+    //         <Route index element={<FoodsPage />} />
+    //         <Route path="/foods/:foodId" element={<FoodDetailPage />} />
+    //       </Route>
+    //       <Route path="orders">
+    //         <Route index element={<MyOrderPage />} />
+    //         <Route path="add" element={<CreateOrderPage />} />
+    //       </Route>
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
