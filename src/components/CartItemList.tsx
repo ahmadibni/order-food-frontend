b3f35bff95d73482e0acc96929ac94e2e63b826a
ahmadibni <ahmadibni007@gmail.com>
2025-07-30 22:49:20 +0700

    style: edit cart list item

--- src/components/CartItemList.tsx
+++ src/components/CartItemList.tsx
@@ -2,6 +2,7 @@ import useCartStore from "../store/useCartStore";
 import { TiPlus, TiMinus } from "react-icons/ti";
 import type { CartItem } from "../types/Cart";
 import { FaTrashCan } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
 
 const CartItemList = ({ item }: { item: CartItem }) => {
   const removeFromCart = useCartStore((state) => state.removeFromCart);
@@ -22,16 +23,22 @@ const CartItemList = ({ item }: { item: CartItem }) => {
   return (
     <div
       key={item.foodId}
-      className="flex items-center space-x-3 p-3 border-[1px] border-dashed border-gray-400 hover:bg-gray-50 rounded-lg transition-colors"
+      className="sm:relative flex items-center space-x-3 p-3 border-[1px] border-dashed border-gray-400 hover:bg-gray-50 rounded-lg transition-colors"
     >
-      <div className="row-span-2 flex items-center">
+      <button
+        onClick={() => removeFromCart(item.foodId)}
+        className="absolute -top-2.5 -right-2.5 z-10 me-0 bg-gray-50 rounded-full p-1.5 shadow-sm hover:bg-gray-200 transition-colors cursor-pointer"
+      >
+        <RxCross2 className="h-3 w-3 text-gray-500" />
+      </button>
+      <div className="flex items-center">
         <img
           src={item.image}
           alt={item.name}
-          className="w-12 h-12 sm:w-16 sm:h-16 rounded-full object-cover"
+          className="w-12 h-12 sm:w-16 sm:h-16 rounded-full object-cover shrink-0"
         />
       </div>
-      <div className="col-span-2">
+      <div>
         <h3 className="font-medium md:text-sm lg:text-base text-gray-900">
           {item.name}
         </h3>
@@ -40,7 +47,7 @@ const CartItemList = ({ item }: { item: CartItem }) => {
         </p>
       </div>
 
-      <div className="ml-auto flex space-x-6">
+      <div className="ml-auto flex space-x-6 sm:space-x-0">
         <div className="flex items-center col-start-3 row-start-2">
           <button
             onClick={() => {
@@ -64,7 +71,7 @@ const CartItemList = ({ item }: { item: CartItem }) => {
         </div>
         <button
           onClick={() => removeFromCart(item.foodId)}
-          className="w-full h-10 px-3 rounded-md flex items-center justify-center transition-colors cursor-pointer"
+          className="w-full sm:hidden h-10 px-3 rounded-md flex items-center justify-center transition-colors cursor-pointer"
         >
           <FaTrashCan className="h-5 w-5 text-red-600" />
         </button>
